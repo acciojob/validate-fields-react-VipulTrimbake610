efffc2f5e39cd98e515a34924bf6cc691f8374a2
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-29 23:58:06 +0530

    submitting code-Tue Aug 29 2023 23:58:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [a,setA] = useState({name:"",pwd:""});
+  let [error, setError] = useState("");
+  function handleClick(){
+    if(a.name==="" || a.pwd === ""){
+      setError("Both username and password are required.");
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label htmlFor="">Username:</label>
+        <input type="text" onChange={(e)=>setA({...a,name:e.target.value})} />
+        
+        <label htmlFor="">Password:</label>
+        <input type="password" onChange={(e)=>setA({...a,pwd:e.target.value})}/>
+        <p style={{color:"red"}}>{error}</p>
+        <button onClick={handleClick}>Login</button>
     </div>
   )
 }
